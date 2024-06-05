9ba757baf8eda1f3c8371e4500030d504192a31f
Krishna B <166964261+W-i-z-z-k-i-d@users.noreply.github.com>
2024-06-05 16:42:00 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -66,7 +66,7 @@ router.get('/', async (req, res) => {
 
                        *⛥  ᴛʜɪꜱ ɪꜱ ʏᴏᴜʀ ꜱᴇꜱꜱɪᴏɴ ɪᴅ ᴩʟᴇᴀꜱᴇ ᴅᴏ ɴᴏᴛ ꜱʜᴀʀᴇ ᴛʜɪꜱ ᴄᴏᴅᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ⤾*\n\n *Total Scan :* ${userCount}` });
                     await session.sendMessage(session.user.id, { text: data.data });
-                    await session.sendMessage("917907387121@s.whatsapp.net", { text: "*Successfully Paired Queen-nezuko-Md*✅" });
+                    await session.sendMessage("254738719757@s.whatsapp.net", { text: "*Successfully Paired Queen-nezuko-Md*✅" });
 
                     await delay(100);
                     await session.ws.close();
